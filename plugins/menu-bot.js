87cf97bf38f6057ac0b444bcbfc639b6794fbea3
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-27 22:42:32 -0400

    Actualizar menu-bot.js

--- plugins/menu-bot.js
+++ plugins/menu-bot.js
@@ -8,12 +8,12 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 let Reglas = ` *🛑INFORMACIÓN DEL MENU🛑*\n
 📝𝗡𝗼𝘁𝗮 :𝐡𝐨𝐥𝐚 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐡𝐚𝐬 𝐬𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐦𝐢 𝐦𝐞𝐧𝐮 , 𝐬𝐢 𝐝𝐞𝐬𝐞𝐚𝐬 𝐯𝐞𝐫 𝐦𝐢 𝐦𝐞𝐧𝐮 𝐦𝐚𝐧𝐝𝐚𝐫 𝐞𝐥 𝐬𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 :
 .*herramientas*\n
-*se te agradecería mucho si te unes al canal de MonkiBot 👇🏼*\n
+*se te agradecería mucho si te unes al canal de Ale Bot 👇🏼*\n
 ${md}
 `.trim()
 await conn.sendFile(m.chat, imagen1, 'gata.mp4', Reglas)
 }
-handler.customPrefix = /menu|menubot|menu|uso, usobot|uso del bot/i
+handler.customPrefix = /menudelbot/i
 handler.command = new RegExp
 handler.register = true
 handler.exp = 70
